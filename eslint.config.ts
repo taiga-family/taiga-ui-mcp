9654f15657bd645d60d7c40e39b4940721cd5990
mdlufy <zidakbew@gmail.com>
2025-10-24 13:42:14 +0300

    chore: update lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,16 @@ import taiga from '@taiga-ui/eslint-plugin-experience-next';
 
 export default [
     ...taiga.configs.recommended,
+    {
+        ignores: [
+            // TypeScript will ignore files with duplicate filenames in the same folder
+            // (for example, src/file.ts and src/file.js). TypeScript purposely ignore
+            // all but one of the files, only keeping the one file
+            // with the highest priority extension
+            '**/eslint.config.ts',
+            '.release-it.js',
+        ],
+    },
 
     {
         files: ['**/*.{js,jsx,ts,tsx}'],
